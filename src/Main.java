633ae90f1c5fbd1baf2e829ade79ced44545e83f
1064064 <boyangs@student.unimelb.edu.au>
2019-08-30 11:30:50 +1000

    Try to commit via idea again

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Boyang World!");
+        System.out.println("Hello Boyang Boyang World!");
     }
 }
